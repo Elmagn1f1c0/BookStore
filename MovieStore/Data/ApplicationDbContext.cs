a0bf05d24a7b9f713d0a4f4ea023879c9bdf8faf
Elmagn1f1c0 <steveamadi007@gmail.com>
2023-12-14 17:25:51 +0100

    SeededCategory

--- MovieStore/Data/ApplicationDbContext.cs
+++ MovieStore/Data/ApplicationDbContext.cs
@@ -8,5 +8,29 @@ namespace MovieStore.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
 
         public DbSet<Category> categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Action",
+                    DisplayOrder = 1
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "SciFi",
+                    DisplayOrder = 2
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Adventure",
+                    DisplayOrder = 3
+                }
+                );
+        }
     }
 }
--- MovieStore/Migrations/20231214162309_SeededData.Designer.cs
+++ MovieStore/Migrations/20231214162309_SeededData.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieStore.Data;
 
@@ -10,9 +11,10 @@ using MovieStore.Data;
 namespace MovieStore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231214162309_SeededData")]
+    partial class SeededData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
