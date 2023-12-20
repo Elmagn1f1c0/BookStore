ee2d1362226cbbbe32e84e2257a15e1b0d5f62c5
Elmagn1f1c0 <steveamadi007@gmail.com>
2023-12-20 20:58:39 +0100

    Dsiplaying all lists

--- MovieStore/Controllers/CategoryController.cs
+++ MovieStore/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieStore.Data;
+using MovieStore.Models;
 
 namespace MovieStore.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> category = _db.categories.ToList();
+            return View(category);
         }
     }
 }
