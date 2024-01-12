43d022133ab22fc5abfbdda2c3b66f3d314b376d
Elmagn1f1c0 <steveamadi007@gmail.com>
2024-01-12 03:31:08 +0100

    Handle Image on update

--- BookStore/Areas/Admin/Controllers/ProductController.cs
+++ BookStore/Areas/Admin/Controllers/ProductController.cs
@@ -10,10 +10,12 @@ namespace BookStore.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -53,6 +55,20 @@ namespace BookStore.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if(file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images/product");
+
+                    using(var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
+
+
                 if (productVM.Product.Id == 0)
                 {
                     _unitOfWork.Product.Add(productVM.Product);
@@ -67,7 +83,7 @@ namespace BookStore.Areas.Admin.Controllers
                 }
                     _unitOfWork.Save();
                     return RedirectToAction("Index");
-                }
+            }
                 else
                 {
                     productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
