c4682bc462e4d13c96788502837448894e776689
Elmagn1f1c0 <steveamadi007@gmail.com>
2024-01-31 21:50:30 +0100

    Dynamic shopping cart

--- BookStore/Areas/Customer/Controllers/ShoppingCartController.cs
+++ BookStore/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -1,4 +1,5 @@
 ﻿using Book.DataAccess.Repository.Interface;
+using Book.Models;
 using Book.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity.UI.Services;
@@ -28,7 +29,29 @@ namespace BookStore.Areas.Customer.Controllers
                 ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties:"Product"),
             };
 
+			foreach(var cart in ShoppingCartVM.ShoppingCartList)
+			{
+				cart.Price = GetPriceBasedOnQuantity(cart);
+				ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+			}
+
             return View(ShoppingCartVM);
 		}
-    }
+
+		private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
+		{
+			if (shoppingCart.Count <= 50)
+			{
+				return shoppingCart.Product.Price;
+			}
+			else
+			{
+				if (shoppingCart.Count <= 100)
+				{
+					return shoppingCart.Product.Price50;
+				}
+				return shoppingCart.Product.Price100;
+			}
+		}
+	}
 }
