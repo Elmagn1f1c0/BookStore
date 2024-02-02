36c5e8c863cad3dc238e43439ff03e3799260ca2
Elmagn1f1c0 <steveamadi007@gmail.com>
2024-02-02 02:48:44 +0100

    Order Summary Ui

--- BookStore/Areas/Customer/Controllers/ShoppingCartController.cs
+++ BookStore/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -39,6 +39,11 @@ namespace BookStore.Areas.Customer.Controllers
 			return View(ShoppingCartVM);
 		}
 
+		public IActionResult Summary()
+		{
+			return View();
+		}
+
 		public IActionResult Plus(int cartId)
 		{
 			var cart = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
