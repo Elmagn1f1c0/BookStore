d5d565bd66ea6e85725c66ad038ce80beef490b9
Elmagn1f1c0 <steveamadi007@gmail.com>
2024-02-20 02:37:16 +0100

    Loading order details

--- BookStore/Areas/Admin/Controllers/OrderController.cs
+++ BookStore/Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using Book.DataAccess.Repository.Interface;
 using Book.Models;
+using Book.Models.ViewModels;
 using Book.Utility;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,6 +21,15 @@ namespace BookStore.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult Details(int orderId)
+        {
+            OrderVM orderVM= new()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product"),
+            };
+            return View(orderVM);
+        }
 
 
         #region API CALLS
